22852a683a45da32f52a9de92ba0694e0a1e2d72
hoanggiang212 <hoanggiang212@gmail.com>
2017-07-06 16:22:52 +0700

    add project upload file to FTP Server
    Add file to fpt server

---
+++ ZEMP/WinApp/UploadFtp/UploadFtp/Common/XmlReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace UploadFtp.Common
+{
+    class XmlReader
+    {
+        XmlDataDocument xmldoc = new XmlDataDocument();
+        public XmlReader()
+        {
+            try
+            {
+                FileStream fs = new FileStream("UploadConfig.xml", FileMode.Open, FileAccess.Read);
+                xmldoc.Load(fs);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+        public string getTagValue(string strParentTag, string strTagName)
+        {
+            string strValue = "";
+            XmlNodeList xmlnode;
+            xmlnode = xmldoc.GetElementsByTagName(strParentTag);
+            for (int i = 0; i <= xmlnode[0].ChildNodes.Count - 1; i++)
+            {
+                xmlnode[0].ChildNodes.Item(i).InnerText.Trim();
+                if (xmlnode[0].ChildNodes.Item(i).Name == strTagName)
+                {
+                    strValue = xmlnode[0].ChildNodes.Item(i).InnerText.Trim();
+                    break;
+                }
+            }
+            return strValue;
+        }
+    }
+}
---
+++ ZEMP/WinApp/UploadFtp/UploadFtp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using UploadFtp.Common;
+
+namespace UploadFtp
+{
+    public partial class Form1 : Form
+    {
+        string srcFolder;
+        string ftpFolder;
+        string strProxy;
+        int iPort;
+        string sUserName;
+        string sPassword;
+        public Form1()
+        {
+            InitializeComponent();
+            ReadConfig();
+            SendFile2Ftp();
+            timer1.Start();
+        }
+        private void ReadConfig()
+        {
+            XmlReader xmlCf = new XmlReader();
+            if (xmlCf != null)
+            {
+                srcFolder = xmlCf.getTagValue("Root", "DataFolder");
+                ftpFolder = xmlCf.getTagValue("Root", "FtpFolder");
+                strProxy = xmlCf.getTagValue("Root", "ProxyIp");
+                iPort = int.Parse(xmlCf.getTagValue("Root", "ProxyPort"));
+                sUserName = xmlCf.getTagValue("Root", "Username");
+                sPassword = xmlCf.getTagValue("Root", "Password");
+            }
+        }
+        
+        private void SendFile2Ftp()
+        {
+            //check exist directory
+            if (!Directory.Exists(srcFolder))
+            {
+                MessageBox.Show("Folder Data does not exist, please check again.");
+                return;
+            }
+
+            string[] listFile = Directory.GetFiles(srcFolder);
+            foreach(string strFileName in listFile)
+            {
+                try
+                {
+                    string sFileName = Path.GetFileName(strFileName);
+                    FtpWebRequest ftp = (FtpWebRequest)FtpWebRequest.Create(ftpFolder + sFileName);
+                    ftp.Credentials = new NetworkCredential(sUserName, sPassword);
+
+                    ftp.KeepAlive = true;
+                    ftp.UseBinary = true;
+
+                    var webProxy = new WebProxy(strProxy, iPort);
+                    ftp.Proxy = webProxy;
+                    ftp.Proxy = null;
+
+                    ftp.Method = WebRequestMethods.Ftp.UploadFile;
+
+
+                    FileStream fs = File.OpenRead(strFileName);
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, buffer.Length);
+                    fs.Close();
+
+                    Stream ftpStream = ftp.GetRequestStream();
+                    ftpStream.Write(buffer, 0, buffer.Length);
+                    ftpStream.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Send file failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendFile2Ftp();
+            timer1.Start();
+        }
+    }
+}
